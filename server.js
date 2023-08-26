df424b57342ff3c6cebe6d40d4feba0910cb6c05
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-27 00:46:45 +0300

    added branches route middleware

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const port = 3000
 
 const loginRoutes = require('./Routes/login')
 const usersRoutes = require('./Routes/users')
+const branchRoutes = require('./Routes/branches')
 
 app.use('/api/users', usersRoutes)
-
+app.use('/api/branches', branchRoutes)
 app.use('/api/login', loginRoutes)
 
 app.listen(port, () => {

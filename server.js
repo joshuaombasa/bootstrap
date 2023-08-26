30d01f06cdf914034254902186f66fc469d2bc08
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-27 00:04:09 +0300

    updated routes

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const port = 3000
 const loginRoutes = require('./Routes/login')
 const usersRoutes = require('./Routes/users')
 
-app.get('/api/users', usersRoutes)
+app.use('/api/users', usersRoutes)
 
-app.get('/api/login', loginRoutes)
+app.use('/api/login', loginRoutes)
 
 app.listen(port, () => {
     console.log(`App listening on ${port}`)

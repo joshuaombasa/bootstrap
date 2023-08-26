e1a162e9d27c938f3359a129947194f4d9ffd549
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-27 00:34:50 +0300

    updated users route

--- Routes/users.js
+++ Routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const mysql = require('mysql2/promise')
+
+const dbConfig = {
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bundaz"
+}
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: "This is home page"})
+router.get('/', async (req, res) => {
+
+    try {
+        const connection = await mysql.createConnection(dbConfig)
+        const getUsersSql = `SELECT * FROM users`
+        const [rows] = await connection.query(getUsersSql)
+        connection.end()
+        res.status(200).json(rows)
+    } catch(error) {
+        Connection.end()
+        return res.status(400).json(error)
+    }
 })
 
 module.exports = router

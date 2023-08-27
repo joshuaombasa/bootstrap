17e68a00934d11c8099328a2c8040b461697a9e5
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-08-27 11:12:54 +0300

    added signup route

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.use(express.json())
 const loginRoutes = require('./Routes/login')
 const usersRoutes = require('./Routes/users')
 const branchRoutes = require('./Routes/branches')
+const signupRoutes = require('./Routes/signup')
 
 app.use('/api/users', usersRoutes)
 app.use('/api/branches', branchRoutes)
 app.use('/api/login', loginRoutes)
+app.use('/api/signup', signupRoutes)
 
 app.listen(port, () => {
     console.log(`App listening on ${port}`)
